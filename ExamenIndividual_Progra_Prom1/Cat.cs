4b339b35842d3b812e31208d34005097835d7135
Melissa Aquije <melisaaquije@gmail.com>
2024-09-22 11:57:05 -0500

    clases hacia dev

---
+++ ExamenIndividual_Progra_Prom1/Cat.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ExamenIndividual_Progra_Prom1
+{
+    class Cat : Enemy
+    {
+        public Cat()
+        {
+            enemyName = "Gato";
+        }
+        public override void AssignValues()
+        {
+                Console.WriteLine($"\nmiau miau");
+                Console.WriteLine($"\nEncontraste a un {enemyName}");
+                Console.WriteLine($"\nInserta su vida >>>");
+
+                health = int.Parse(Console.ReadLine());
+
+                Console.WriteLine("\nInserta su ataque >>>");
+
+                attack = int.Parse(Console.ReadLine());
+
+                Console.WriteLine($"Enemigo {enemyName} creado con {health} de vida y {attack} de ataque");
+        }
+    }
+}
---
+++ ExamenIndividual_Progra_Prom1/Inventory.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ExamenIndividual_Progra_Prom1
+{
+    public class Inventory<T>
+    {
+        private List<T> items = new List<T>();
+        public void Add(T item)
+        {
+            items.Add(item);
+            Console.WriteLine($"Lo has añadido al inventario ~");
+        }
+
+        public void ShowItems()
+        {
+            Console.WriteLine("Tu inventario contiene:");
+            if (items.Count == 0)
+            {
+                Console.WriteLine("Aun no tienes nada en tu inventario :(");
+                return;
+            }
+
+            foreach (var item in items)
+            {
+                Console.WriteLine(item.ToString());
+            }
+        }
+    }
+}
